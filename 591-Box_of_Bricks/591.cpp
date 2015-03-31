aaf41c0bd9a696abd7b618b89e00995ebca6a873
rox38431 <rox38431@gmail.com>
2015-03-31 21:55:12 +0800

    that's all

---
+++ 591-Box_of_Bricks/591.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main ()	{
+	int stack[50], stack_num, count = 1;
+	
+	while (cin >> stack_num && stack_num != 0)	{
+		int aver = 0, ans = 0;
+
+		for (int i = 0; i < stack_num; i++)	{
+			cin >> stack[i];
+			aver += stack[i];
+		}
+	
+		aver /= stack_num;
+		cout << aver << endl;
+		for ( int i = 0; i < stack_num; i++)	{
+			if (stack[i] > aver)
+				ans += stack[i] - aver;
+		}
+		
+		cout << "Set #" << count++ << endl;
+		cout << "The minimum number of moves is " << ans << "." << endl << end;
+	}
+
+	return 0;
+}			
+		
